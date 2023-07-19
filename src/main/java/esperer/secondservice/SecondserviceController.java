8503880f12d3157fbb8f0cff0238f253e384387a
aesperer <piyrw9754@gmail.com>
2023-07-19 20:50:19 +0900

    log header

--- src/main/java/esperer/secondservice/SecondserviceController.java
+++ src/main/java/esperer/secondservice/SecondserviceController.java
@@ -1,9 +1,12 @@
 package esperer.secondservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/second-service")
 public class SecondserviceController {
@@ -11,5 +14,11 @@ public class SecondserviceController {
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to a Second Service";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("second-request") String header){
+        log.info(header);
+        return "Hello World in Second Service";
     }
 }
